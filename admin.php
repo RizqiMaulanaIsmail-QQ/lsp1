67a4925cdc5cae3b40f5736b60d4f0ab6a7f7ef7
RizqiMaulanaIsmail-QQ <127165154+RizqiMaulanaIsmail-QQ@users.noreply.github.com>
2023-11-14 23:16:20 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/admin.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h4>E-book</h4>
+            <img src="img/logo.png" alt="">
+        </div>
+        
+        <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="profile.php">Profil</a></li>
+            <li><a href="kategori.php">Kategori</a></li>
+            <li><a href="aktivitas.php">Aktivitas</a></li>
+            
+            <li><a href="logout.php" class="button">Logout</a></li>
+        </ul>
+
+        <div class="list-menu">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ kategori.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/kategori.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h4>E-book</h4>
+            <img src="img/logo.png" alt="">
+        </div>
+        
+        <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="profile.php">Profil</a></li>
+            <li><a href="kategori.php">Kategori</a></li>
+            <li><a href="aktivitas.php">Aktivitas</a></li>
+            
+            <li><a href="logout.php" class="button">Logout</a></li>
+        </ul>
+
+        <div class="list-menu">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+    <div class="kategori">
+        <div class="mapel">
+            <div class="mapel-judul">
+                <p>Buku Pelajaran</p>
+            </div>
+            <div class="container-buku">
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+            </div>
+        </div>
+        <div class="mapel">
+            <div class="mapel-judul">
+                <p>Buku Fiksi</p>
+            </div>
+            <div class="container-buku">
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+            </div>
+        </div>
+        <div class="mapel">
+            <div class="mapel-judul">
+                <p>Buku Biografi</p>
+            </div>
+            <div class="container-buku">
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+                <div class="isi-buku">
+                    <img src="img/mapel1.jpg" alt="">
+                    <button type="submit" name="logout">Detail</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
